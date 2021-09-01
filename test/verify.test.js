188b6a19260b8d300c1e9d43ee7e84ac8dd6b1cf
Jared Hanson <jaredhanson@gmail.com>
2021-09-01 11:30:19 -0700

    Add verify test.

---
+++ test/verify.test.js
@@ -0,0 +1,54 @@
+/* global describe, it */
+
+var chai = require('chai')
+  , sinon = require('sinon')
+  , Strategy = require('../lib/strategy');
+
+
+describe('Strategy', function() {
+  
+  describe.only('handling a valid credentials', function() {
+    var verify = sinon.spy(function(id, cb) {
+      var publicKey = '-----BEGIN PUBLIC KEY-----\n' +
+'MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAE/6j9N8S3dBWutrvVJBB3MrU5uipV\n' +
+'D8+rZ0GboVEJMPT3HZmICG/06CAPSqcDchP+qLa0N8Tvp9FSmguCnvLtZg==\n' +
+'-----END PUBLIC KEY-----';
+      
+      return cb(null, { id: '500' }, publicKey);
+    });
+    
+    var strategy = new Strategy(verify);
+    var user;
+    
+    before(function(done) {
+      chai.passport(strategy)
+        .success(function(u) {
+          user = u;
+          done();
+        })
+        .req(function(req) {
+          req.body = {
+            rawId: 'JYrR3EvvQJNqG0i_OwJckOkbzq4YJWviotG4hig9wA_Qdxm-eBEHfsYqBJKTtXMasL-RD9CFOlcag48icK3E8Q',
+            response: {
+              authenticatorData: 'SZYN5YgOjGh0NBcPZHZgW4_krrmihjLHmVzzuoMdl2MBAAAANw',
+              signature: 'MEYCIQDfGFxZvtHzGZUzfgxIjD4BaF1YF8BKOSGwk-rVkydv3wIhAOZn4lufNS5zptLpfHf-6YPcauXW5MJW_UVnVIHvPzGD',
+              userHandle: null,
+              clientDataJSON: 'eyJ0eXBlIjoid2ViYXV0aG4uZ2V0IiwiY2hhbGxlbmdlIjoiTlRZM09BIiwib3JpZ2luIjoiaHR0cDovL2xvY2FsaG9zdDozMDAwIiwiY3Jvc3NPcmlnaW4iOmZhbHNlLCJvdGhlcl9rZXlzX2Nhbl9iZV9hZGRlZF9oZXJlIjoiZG8gbm90IGNvbXBhcmUgY2xpZW50RGF0YUpTT04gYWdhaW5zdCBhIHRlbXBsYXRlLiBTZWUgaHR0cHM6Ly9nb28uZ2wveWFiUGV4In0'
+            },
+            id: 'JYrR3EvvQJNqG0i_OwJckOkbzq4YJWviotG4hig9wA_Qdxm-eBEHfsYqBJKTtXMasL-RD9CFOlcag48icK3E8Q',
+            type: 'public-key'
+          };
+        })
+        .authenticate();
+    });
+    
+    it('should verify assertion', function() {
+      expect(verify).to.be.calledWith('JYrR3EvvQJNqG0i_OwJckOkbzq4YJWviotG4hig9wA_Qdxm-eBEHfsYqBJKTtXMasL-RD9CFOlcag48icK3E8Q');
+    });
+    
+    it('should supply user', function() {
+      expect(user).to.deep.equal({ id: '500' });
+    });
+  });
+  
+});
