903ac132f8a806c21ff28dd563fbb4f83b52405f
Jared Hanson <jaredhanson@gmail.com>
2022-05-31 15:54:02 -0700

    Stubbing out some registration.

--- test/strategy.test.js
+++ test/strategy.test.js
@@ -6,9 +6,45 @@ var Strategy = require('../lib/strategy');
 describe('Strategy', function() {
   
   it('should be named webauthn', function() {
-    var strategy = new Strategy(function() {});
+    var strategy = new Strategy(function(){});
     
     expect(strategy.name).to.equal('webauthn');
+  });
+  
+  
+  it.only('should do something to register', function(done) {
+  
+    var strategy = new Strategy(function(){ console.log('VERIFY!'); }, function(){ console.log('REGISTER!');});
+  
+    chai.passport.use(strategy)
+      .request(function(req) {
+        console.log('REQ1');
+        
+        req.connection = {};
+        req.headers.host = 'localhost:3000';
+        req.body = {
+          "rawId": "n90ZI-FwPA9HT5jtZins33Rtae1Zz1zLnoDR9yCj5Jwz2PB6fJR0KCPZehORB-ht48mRfbcA512cnDyfbQQ0OQ",
+          "response": {
+            "attestationObject": "o2NmbXRkbm9uZWdhdHRTdG10oGhhdXRoRGF0YVjESZYN5YgOjGh0NBcPZHZgW4_krrmihjLHmVzzuoMdl2NBAAAAOgAAAAAAAAAAAAAAAAAAAAAAQJ_dGSPhcDwPR0-Y7WYp7N90bWntWc9cy56A0fcgo-ScM9jwenyUdCgj2XoTkQfobePJkX23AOddnJw8n20ENDmlAQIDJiABIVggp-ZH3WpsBVUojVZ1D1BufQoSMplIVbiKl-g4PojQCfkiWCBkt-eEtwaQkv2aRTTLMVHr0gucag7QbOA0m_WyFtuXIg",
+            "clientDataJSON": "eyJ0eXBlIjoid2ViYXV0aG4uY3JlYXRlIiwiY2hhbGxlbmdlIjoiTVRJek5BIiwib3JpZ2luIjoiaHR0cDovL2xvY2FsaG9zdDozMDAwIiwiY3Jvc3NPcmlnaW4iOmZhbHNlfQ"
+          },
+          "id": "n90ZI-FwPA9HT5jtZins33Rtae1Zz1zLnoDR9yCj5Jwz2PB6fJR0KCPZehORB-ht48mRfbcA512cnDyfbQQ0OQ",
+          "type": "public-key"
+        };
+        
+      })
+      .success(function(user, info) {
+        console.log('SUCCESS');
+        console.log(user);
+        console.log(info);
+        
+        expect(user).to.deep.equal({ id: '248289761001' });
+        expect(info).to.deep.equal({});
+        done();
+      })
+      .error(done)
+      .authenticate();
+  
   });
   
 });
